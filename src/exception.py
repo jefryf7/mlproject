58066a66203db512103a39e409dfe1b5dca5ab7d
jefryf7 <jefryfredi07@gmail.com>
2023-07-04 12:03:02 +0800

    Data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,13 +1,15 @@
 import sys
-
+from src.logger import logging
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
+    #eror mengambil file saat ini, dan baris kesalahan kode, dan error yang didapat
     error_message = f'Error occured in python scipt name {file_name} Line number {exc_tb.tb_lineno} error message {str(error)}'
     
     return error_message
 
 class CustumException(Exception):
+    #error_detail adalah sebuaah objek dari sys
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message,error_detail)
@@ -15,8 +17,3 @@ class CustumException(Exception):
     def __str__(self):
         return self.error_message
     
-if __name__ =='__main__':
-    try:
-        a = 1/0
-    except Exception as e:
-        raise CustumException(e,sys)
--- src/logger.py
+++ src/logger.py
@@ -2,20 +2,21 @@ import logging
 import os
 from datetime import datetime
 
+#ini akan membuat sebuah nama file yang akan berbeda, karena kita memasukkan sebauh detik di akhir
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+#membuat folder
 log_path = os.path.join(os.getcwd(),'logs',LOG_FILE)
+
+#jika folder logs sudah ada sebelumnya, tambahkan folder baru di dalam logs tersebut
 os.makedirs(log_path,exist_ok=True)
 
+#ini adalah sebuah nama filenya, yang akan kit gabungkan dengan log_path
 LOG_FILE_PATH = os.path.join(log_path,LOG_FILE)
+print('tes',LOG_FILE_PATH)
 
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format= "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
+    filename=LOG_FILE_PATH,#nama sebuah file
+    format= "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",#isi dari sebuah file
+    level=logging.INFO,#memberikan sebuah informasi 
 )
-
-
-if __name__ =='__main__':
-    logging.info('logginf has started')
-
-
